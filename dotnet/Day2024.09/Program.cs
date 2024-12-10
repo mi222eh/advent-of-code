5f58bf052fe9325b174ae245de1c560b8ae79abe
mingvarsson13 <156089673+mingvarsson13@users.noreply.github.com>
2024-12-10 20:23:10 +0100

    day9

--- dotnet/Day2024.09/Program.cs
+++ dotnet/Day2024.09/Program.cs
@@ -1,34 +1,34 @@
-﻿List<Entity> entities = Common.ReadInputDefault().SelectMany((digitCh, index) =>
-{
-    long ID = 0;
-    var digit = int.Parse(digitCh.ToString());
-    var isFile = index % 2 == 0;
+﻿// List<Entity> entities = Common.ReadInputDefault().SelectMany((digitCh, index) =>
+// {
+//     long ID = 0;
+//     var digit = int.Parse(digitCh.ToString());
+//     var isFile = index % 2 == 0;
 
-    if (isFile)
-    {
-        ID = Utils.GetNextId();
-    }
-    List<Entity> entities = [];
-    while (entities.Count < digit)
-        entities.Add(isFile ? new File(ID) : new Empty());
+//     if (isFile)
+//     {
+//         ID = Utils.GetNextId();
+//     }
+//     List<Entity> entities = [];
+//     while (entities.Count < digit)
+//         entities.Add(isFile ? new File(ID) : new Empty());
 
-    return entities;
-}).ToList();
+//     return entities;
+// }).ToList();
 
-while (entities.CountEmptyRight() != entities.CountEmpty())
-{
-    var nextEmpty = entities.FindIndex(x => x is Empty);
-    var nextFile = entities.FindLastIndex(x => x is File);
-    entities.SwapIndexes(nextEmpty, nextFile);
-}
+// while (entities.CountEmptyRight() != entities.CountEmpty())
+// {
+//     var nextEmpty = entities.FindIndex(x => x is Empty);
+//     var nextFile = entities.FindLastIndex(x => x is File);
+//     entities.SwapIndexes(nextEmpty, nextFile);
+// }
 
-var sum = entities.TakeWhile(x => x is File).Select(static (f, i) => (f as File).ID * i).Sum();
+// var sum = entities.TakeWhile(x => x is File).Select(static (f, i) => (f as File).ID * i).Sum();
 
-Console.WriteLine(sum);
+// Console.WriteLine(sum);
 
 // PART 2
 Utils.ResetID();
-entities = Common.ReadInputDefault().SelectMany((digitCh, index) =>
+var entities2 = Common.ReadInputDefault().SelectMany((digitCh, index) =>
 {
     long ID = 0;
     var digit = int.Parse(digitCh.ToString());
@@ -45,23 +45,31 @@ entities = Common.ReadInputDefault().SelectMany((digitCh, index) =>
     return entities;
 }).ToList();
 
+System.IO.File.WriteAllText("before.txt", entities2.print());
+var uniqueIds = entities2.GetUniqueIds().Order().Reverse().ToList();
 
-System.IO.File.WriteAllText("before.txt", entities.print());
-foreach (var fileBlock in entities.IterFiles())
+
+foreach (var FileID in uniqueIds)
 {
-    var matches = entities.IterEmptyBlocks().TakeWhile(x => x.Last() < fileBlock.Last()).Where((x) => x.Count >= fileBlock.Count).ToList();
-    if (matches.Count == 0) continue;
-    var matched = matches.First();
-    for (int i = 0; i < fileBlock.Count; i++)
+    Console.WriteLine($"[{FileID}] Handling");
+    var files = entities2.GetFilesWithIndexById(FileID);
+    var match = entities2.GetEmptyBlocks().ToList().Find((x) => x.Count >= files.Count);
+    if (match is null)
     {
-        entities.SwapIndexes(matched[i], fileBlock[i]);
+        Console.WriteLine($"[{FileID}] No Matches");
+        continue;
+    }
+    Console.WriteLine($"[{FileID}] Found Matches");
+    for (int i = 0; i < files.Count; i++)
+    {
+        entities2.SwapIndexes(match[i], files[i].index);
     }
 }
-System.IO.File.WriteAllText("after.txt", entities.print());
+System.IO.File.WriteAllText("after.txt", entities2.print());
 
 
 
-var sum2 = entities.Select(static (f, i) =>
+var sum2 = entities2.Select(static (f, i) =>
 {
     if (f is File file)
     {
@@ -117,8 +125,8 @@ static class Utils
 
     public static IEnumerable<List<int>> IterFiles(this List<Entity> entities)
     {
-        List<int> fileBlock = new();
-        List<File> fileBlockFiles = new();
+        List<int> fileBlock = [];
+        List<File> fileBlockFiles = [];
         for (int i = entities.Count - 1; i >= 0; i--)
         {
             var entity = entities[i];
@@ -127,8 +135,8 @@ static class Utils
                 if (fileBlock.Count > 0)
                 {
                     yield return fileBlock;
-                    fileBlock = new();
-                    fileBlockFiles = new();
+                    fileBlock = [];
+                    fileBlockFiles = [];
                 }
             }
 
@@ -137,8 +145,8 @@ static class Utils
                 if (!fileBlockFiles.All(x => x.ID == file.ID))
                 {
                     yield return fileBlock;
-                    fileBlock = new();
-                    fileBlockFiles = new();
+                    fileBlock = [];
+                    fileBlockFiles = [];
                 }
 
                 fileBlock.Add(i);
@@ -147,27 +155,38 @@ static class Utils
         }
     }
 
-    public static IEnumerable<List<int>> IterEmptyBlocks(this List<Entity> entities)
+    public static IEnumerable<List<int>> GetEmptyBlocks(this List<Entity> entities)
     {
-        List<int> emptyBlock = new();
-        for (int i = 0; i < entities.Count; i++)
+        var currentIndex = 0;
+        while (true)
         {
-            var entity = entities[i];
-            if (entity is File)
-            {
-                if (emptyBlock.Count > 0)
-                {
-                    yield return emptyBlock;
-                    emptyBlock = new();
-                }
-            }
-            if (entity is Empty empty)
-            {
-                emptyBlock.Add(i);
-            }
-        }
+            currentIndex = entities.FindIndex(currentIndex, x => x is Empty);
+            if (currentIndex < 0)
+                break;
+            var size = entities.Skip(currentIndex).TakeWhile(x => x is Empty).Count();
+            yield return Enumerable.Range(currentIndex, size).ToList();
+            currentIndex += size;
 
+
+        }
     }
+
+    public static List<File> GetFiles(this List<Entity> entities) => entities.OfType<File>().ToList();
+    public static List<(File file, int index)> GetFilesWithIndex(this List<Entity> entities)
+    {
+        return entities
+            .WithIndex()
+            .Where(x => x.Item1 is File)
+            .Select(x => ((File)x.Item1, x.Item2))
+            .ToList();
+    }
+    public static List<(File file, int index)> GetFilesWithIndexById(this List<Entity> entities, long ID)
+    {
+        return entities.GetFilesWithIndex().Where(x => x.file.ID == ID).ToList();
+    }
+    public static List<(Entity, int)> WithIndex(this List<Entity> entities) => entities.Select((v, i) => (entity: v, index: i)).ToList();
+    public static List<File> GetFilesByID(this List<Entity> entities, long ID) => entities.GetFiles().Where(x => x.ID != ID).ToList();
+    public static List<long> GetUniqueIds(this List<Entity> entities) => entities.GetFiles().Select(x => x.ID).Distinct().ToList();
 
     public static string print(this List<Entity> entities)
     {
